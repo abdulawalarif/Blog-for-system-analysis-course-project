714dc5153eff5f6b27f365e5d368fac628dc7ae5
abdulawalarif <ashikbdc@gmail.com>
2018-12-02 21:51:33 +0600

    This is the first commit for blog project

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/awal/Desktop/163 Blog-Clone-Project/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This is a Django project for the purpose of course System analysis.
